f15abf943d779ad0be7d6bee791a1c68aabde920
Connor Jessup <cjessup@umich.edu>
2018-04-27 12:04:21 -0400

    more of IngredientList component

---
+++ components/IngredientList.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+// import Ingredient component
+import Ingredient from './Ingredient';
+
+const IngredientList = () => (
+  <View style={styles.container}>
+    {
+      
+    }
+  </View>
+)
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  }
+})
+
+export default IngredientList;
