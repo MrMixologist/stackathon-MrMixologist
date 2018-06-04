bc7daf0387b0b3080e8ca510d2d5de1df81d8619
Val Pilossof <valpil610@gmail.com>
2018-06-04 12:08:16 -0400

    ADDED: began login functionality

---
+++ components/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as CocktailMatches } from './CocktailMatches';
+export { default as IngredientList } from './IngredientList';
+export { default as Login } from './Login';
+export { default as SingleCocktail } from './SingleCocktail';
